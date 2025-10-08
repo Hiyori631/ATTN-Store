aaff6eb5b87b83904cc3df274088eec7f9e81603
trishtianmisc <118181454+trishtianmisc@users.noreply.github.com>
2025-10-08 12:44:55 +0800

    giilisan ni trishtian
    * Try wow change

--- ATTN_React/src/App.jsx
+++ ATTN_React/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
           <h1 className="heritage-text text-white text-3xl sm:text-4xl md:text-[72px] font-bold drop-shadow-lg">
 ATTN          </h1>
           <h1 className="heritage-text text-white text-3xl sm:text-4xl md:text-[72px] font-bold drop-shadow-lg">
-            STORE HEY
+            STORE WOW
           </h1>
 
           {/* ✅ Dynamic message from Django */}
