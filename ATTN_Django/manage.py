135a33577fd5867835e9f3ef4460fe887772e84f
Pia <153894026+piadesu@users.noreply.github.com>
2025-10-08 12:52:00 +0800

    Pia's branch (#4)
    * added comment now in this branch

--- ATTN_Django/manage.py
+++ ATTN_Django/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-#comment now
+#comment now in this branch
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ATTN_Django.settings')
