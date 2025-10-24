6f32251732ca47eb0dd2f8a8f618a069bf51555c
Wolve <wolverineneilrondina@gmail.com>
2025-10-24 23:07:10 +0800

    routing of the pages

---
+++ ATTN_React/src/pages/inventory_add_product.jsx
@@ -0,0 +1,11 @@
+function AddProduct() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-[#4D1C0A] mb-4">Add Product</h1>
+      <p>This is your Add Product page.</p>
+    </div>
+  );
+}
+
+export default AddProduct;
+//yeahh
